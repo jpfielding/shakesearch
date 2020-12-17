66d9fefd67e7ab01fac4c39f5fd27f84cd8c1757
JP Fielding <jp.fielding@gmail.com>
2020-12-17 00:38:01 +0000

    simple validations

--- pkg/search/searcher_test.go
+++ pkg/search/searcher_test.go
@@ -6,11 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSearcher(t *testing.T) {
-	searcher := Searcher{}
+func TestHamlets(t *testing.T) {
+	searcher := Searcher{Width: 10}
 	err := searcher.Load("../../completeworks.txt")
 	assert.Nil(t, err)
 
 	hamlets := searcher.Search("Hamlet")
 	assert.Equal(t, 111, len(hamlets))
+}
+
+func TestWhereForArtThous(t *testing.T) {
+	searcher := Searcher{Width: 10}
+	err := searcher.Load("../../completeworks.txt")
+	assert.Nil(t, err)
+
+	hamlets := searcher.Search("herefore art thou")
+	assert.Equal(t, 1, len(hamlets))
 }
